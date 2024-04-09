f0f90cf31735f29e03733275b5f3302d69a107f3
carloseduardo31 <carlos.eduardo.3832@gmail.com>
2024-04-09 17:20:09 -0300

    Implemenado Sessão Depoimentos portugues.

--- assets/js/main.js
+++ assets/js/main.js
@@ -192,34 +192,23 @@
     selector: '.gallery-lightbox'
   });
 
-  /**
+    /**
    * Testimonials slider
    */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 40
+    new Swiper('.testimonials-slider', {
+      speed: 600,
+      loop: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
       },
-
-      1200: {
-        slidesPerView: 2,
-        spaceBetween: 40
+      slidesPerView: 'auto',
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
       }
-    }
-  });
+    });
 
   /**
    * Animation on scroll
